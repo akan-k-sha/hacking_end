2d937cdf47cba3c067e84d088e8cab361d504e3d
akan-k-sha <67052606+akan-k-sha@users.noreply.github.com>
2022-10-29 16:06:03 +0530

    recursion1

---
+++ first.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void rec(int index,vector<int>nums,vector<vector<int>>&ans){
+        if(index==nums.size()){
+            ans.push_back(nums);
+            return;
+        }
+        
+        for(int i=index;i<nums.size();i++){
+            swap(nums[i],nums[index]);
+            rec(index+1,nums,ans);
+            swap(nums[i],nums[index]);
+        }
+    }
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>>ans;
+        rec(0,nums,ans);
+        return ans;
+    }
+};
